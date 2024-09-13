7e2f7f3129c71ae7d7e48a3f84e626f26995e372
melton-jason <meltonjason12@yahoo.com>
2024-09-13 17:03:22 -0500

    Resolve spacing between boards

--- src/board.py
+++ src/board.py
@@ -3,11 +3,11 @@ from typing import Optional
 from pygame import Surface
 
 from .types import Coordinate
-from .cell import Cell
+from .cell import Cell, px_to_cell_index
 
 class Board:
-    def __init__(self, y_offset, board_size=10) -> None:
-        self.cells = [Cell(x, y + y_offset, board_size) for x in range(board_size)
+    def __init__(self, y_offset, width, height, board_size) -> None:
+        self.cells = [Cell(x, y + px_to_cell_index(y_offset, height, board_size), board_size, width, height) for x in range(board_size)
                       for y in range(board_size)]
 
     def draw(self, surface: Surface):
@@ -15,6 +15,15 @@ class Board:
             cell.draw(surface)
     
     def hit_pos(self, coord: Coordinate) -> Optional[Cell]: 
+        """
+        Checks whether the given coordinate hits a cell in the board. "Hits" and returns the cell if hit, 
+        or returns None to indiciate no cells were hit 
+
+        Args: 
+            coord (Tuple[int, int]): The x and y coordinate of the hit in px
+        Returns: 
+            Cell or None: The Cell hit by the coordinate, or None if no cells in the board were hit
+        """
         for cell in self.cells: 
             if cell.hit(coord):
                 return cell
--- src/game.py
+++ src/game.py
@@ -2,30 +2,47 @@ import sys
 
 import pygame
 
-from .config import FPS
+from .config import FPS, SCREEN_WIDTH, SCREEN_HEIGHT
 
 from .board import Board
+from .cell import cell_width
+
+HALF_HEIGHT = SCREEN_HEIGHT / 2
 
 
 class Game:
-    def __init__(self, clock, surface: pygame.Surface, grid_size) -> None:
+    def __init__(self, clock, surface: pygame.Surface, grid_size: int) -> None:
         self._running = True
         self.clock = clock
         self.surface = surface
-        self.ships = Board(y_offset=0, board_size=grid_size)
-        self.opponent = Board(y_offset=grid_size, board_size=grid_size)
+        self.ships = Board(y_offset=0, width=SCREEN_WIDTH,
+                           height=HALF_HEIGHT, board_size=grid_size)
+
+        # Put 1/3 cell worth of space in between the two boards
+        third_cell = (cell_width(HALF_HEIGHT, grid_size) / 3)
+        self.opponent = Board(y_offset=HALF_HEIGHT + third_cell, width=SCREEN_WIDTH,
+                              height=HALF_HEIGHT - third_cell, board_size=grid_size)
 
     def run(self):
+        """
+        Starts the Game and handles the core game loop
+        """
         while self._running:
             self.render()
             self.handle_events()
             self.handle_update()
 
     def render(self):
+        """
+        Handles drawing and showing all UI
+        """
         self.ships.draw(self.surface)
         self.opponent.draw(self.surface)
 
     def handle_events(self):
+        """
+        Handles catching and processing events which happen each frame: i.e., game logic
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._running = False
@@ -37,5 +54,8 @@ class Game:
                 self.opponent.hit_pos(mouse_pos)
 
     def handle_update(self):
+        """
+        Handles advancing the game state
+        """
         pygame.display.update()
         self.clock.tick(FPS)
