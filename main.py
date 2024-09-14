51881260b4a1bca6634afae3c0af3c632c0fbef1
noahogrady <nmogrady@ku.edu>
2024-09-14 01:32:03 -0500

    Removed Test Code From main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from pygame.locals import *
-import src.display
 
 from src.display import initialize_game_window, FPS
 
@@ -16,8 +15,7 @@ def handle_update():
 
 def game_loop():
     handle_update()
-    src.display.game_over_display(GAME_WINDOW, 1000) #function to display game over screen
-    src.display.game_over_display(GAME_WINDOW, 2) #function to display game over screen
+
 if __name__ == "__main__":
     while True: 
         game_loop()
